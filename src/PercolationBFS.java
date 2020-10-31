f924decbce2c0b871d2ef94397898daf3c86c2fd
TimothyGeissler <timothy.geissler@duke.edu>
2020-10-31 15:17:42 -0400

    PercolationDFSFast.java working 4/4

---
+++ src/PercolationBFS.java
@@ -0,0 +1,47 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PercolationBFS extends PercolationDFSFast{
+    /**
+     * Initialize a grid so that all cells are blocked.
+     *
+     * @param n is the size of the simulated (square) grid
+     */
+    public PercolationBFS(int n) {
+        super(n);
+    }
+
+    @Override
+    protected void dfs(int row, int col) {
+        Queue<int []> queue = new LinkedList<>();
+        myGrid[row][col] = FULL;
+        queue.add(new int[]{row, col});
+
+        while (queue.size() != 0) {
+            int [] p = queue.remove();
+
+            try {
+                if (!isFull(p[0], p[1] + 1) && isOpen(p[0], p[1] + 1)) {
+                    myGrid[p[0]][p[1] + 1] = FULL;
+                    queue.add(p);
+                }
+                if (!isFull(p[0], p[1] - 1) && isOpen(p[0], p[1] - 1)) {
+                    myGrid[p[0]][p[1] - 1] = FULL;
+                    queue.add(p);
+                }
+                if (!isFull(p[0] - 1, p[1]) && isOpen(p[0] - 1, p[1])) {
+                    myGrid[p[0] - 1][p[1]] = FULL;
+                    queue.add(p);
+                }
+                if (!isFull(p[0] + 1, p[1]) && isOpen(p[0] + 1, p[1])) {
+                    myGrid[p[0] + 1][p[1]] = FULL;
+                    queue.add(p);
+                }
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("On edge");
+            }
+        }
+
+        //super.dfs(row, col);
+    }
+}
---
+++ src/PercolationDFSFast.java
@@ -0,0 +1,57 @@
+public class PercolationDFSFast extends PercolationDFS {
+
+    /**
+     * Initialize a grid so that all cells are blocked.
+     *
+     * @param n is the size of the simulated (square) grid
+     */
+    public PercolationDFSFast(int n) {
+        super(n);
+    }
+
+    @Override
+    protected void updateOnOpen(int row, int col) {
+        boolean doDFS = false;
+        if (row == 0) {
+            doDFS = true;
+        }
+        try {
+            if (isFull(row, col + 1)) {
+                doDFS = true;
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("On edge");
+        }
+        try {
+            if (isFull(row, col - 1)) {
+                doDFS = true;
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("On edge");
+        }
+        try {
+            if (isFull(row - 1, col)) {
+                doDFS = true;
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("On edge");
+        }
+        try {
+            if (isFull(row + 1, col)) {
+                doDFS = true;
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("On edge");
+        }
+        /*try {
+            if (row == 0 || isFull(row, col + 1) || isFull(row, col - 1) || isFull(row - 1, col) || isFull(row + 1, col)) {
+                dfs(row, col);
+            }
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("On edge");
+        }*/
+        if (doDFS) {
+            dfs(row, col);
+        }
+    }
+}
